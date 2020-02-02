d0de05c2d2effaa5a204cc2f22d4529244c2d1a9
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-01 23:27:24 -0600

    Structured basic components and set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // React
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Components
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 // Styling
 import './App.css';
 
@@ -10,6 +14,13 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
