741860eb928bbec751eeb19b3b72bf7be314bd5a
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-02 00:24:12 -0600

    Added styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,14 @@ import {
 const Navbar = () => {
   return (
     <div className='nav-cont'>
-      <Jumbotron inverse style={{color: "black", background: "rgba(0, 0, 0, 0.3)"}}>
+      <Jumbotron inverse style={{color: "rgb(69,69,69)", background: "rgba(0, 0, 0, 0.3)", fontFamily: "'Roboto Mono', monospace"}}>
         <h1 className="display-3">Lyndsi Kay Williams</h1>
         <hr className="my-2" />
         <p className="lead">Full Stack Web Developer</p>
         <Nav style={{justifyContent: "center", marginTop: "2%"}}>
-          <NavLink href="/" active style={{color: "black", fontSize: "1.5rem", padding: ".5rem 5rem"}}>About</NavLink>
-          <NavLink href="/projects" style={{color: "black", fontSize: "1.5rem", padding: ".5rem 5rem"}}>Projects</NavLink>
-          <NavLink href="/contact" style={{color: "black", fontSize: "1.5rem", padding: ".5rem 5rem"}}>Contact</NavLink>
+          <NavLink href="/" active style={{color: "rgb(69,69,69)", fontSize: "1.5rem", padding: ".5rem 5rem"}}>About</NavLink>
+          <NavLink href="/projects" style={{color: "rgb(69,69,69)", fontSize: "1.5rem", padding: ".5rem 5rem"}}>Projects</NavLink>
+          <NavLink href="/contact" style={{color: "rgb(69,69,69)", fontSize: "1.5rem", padding: ".5rem 5rem"}}>Contact</NavLink>
         </Nav>
       </Jumbotron>
     </div>
